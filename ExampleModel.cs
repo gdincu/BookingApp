c6005fb6e0bbe217202ec1bc8e5558f737484e43
Ionut Dincu <id@beveco.nl>
2022-04-10 15:47:11 +0300

    20220410 - First commit

---
+++ ExampleModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace BookingApp
+{
+    public class ExampleModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Client { get; set; }
+        public string Frizer { get; set; }
+        public DateTime Data { get;set; }
+        public string Comentarii { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+}
